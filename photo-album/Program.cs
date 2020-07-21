1a0cde34c45be47943a5646e6e13539dd00d72c0
UnicornOfMagic <jacobtfeld@gmail.com>
2020-07-20 20:41:27 -0500

    init commit

---
+++ photo-album/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace photo_album
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
