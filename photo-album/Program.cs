8b627e7c834cc3de0088bb9c2d6ab64f85e5f1be
UnicornOfMagic <jacobtfeld@gmail.com>
2020-07-21 19:24:39 -0500

    Program updates
    Quality of Life updates to the main program execution for more clear error messaging

--- photo-album/Program.cs
+++ photo-album/Program.cs
@@ -9,7 +9,18 @@ namespace photo_album
             var photoService = new JSonPlaceHolderPhotoService();
             var photoRetriever = new PhotoRetriever(photoService);
 
-            if (!int.TryParse(args[0], out var value)) return;
+            if (args.Length == 0)
+            {
+                Console.WriteLine(
+                    "Missing Parameter. Valid usage is a single integer between 1-100 selecting an album.");
+                return;
+            }
+            if(!int.TryParse(args[0], out var value )|| value > 100 || value < 1)
+            {
+                Console.WriteLine(
+                    "Invalid Parameter. Valid usage is a single integer between 1-100 selecting an album.");
+                return;
+            }
 
             var photos = photoRetriever.GetPhotosByAlbumId(value).GetAwaiter().GetResult();
             
